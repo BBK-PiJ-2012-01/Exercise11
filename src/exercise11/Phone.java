00fb053f35a762986c7b8d83613ea799c09bd56b
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-11-05 13:38:46 +0000

    Initial commit

---
+++ src/exercise11/Phone.java
@@ -0,0 +1,19 @@
+package exercise11;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eatmuchpie
+ * Date: 05/11/2012
+ * Time: 13:34
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * A phone makes calls
+ */
+public interface Phone {
+    /**
+     * Just print on the screen: "Calling <number>...".
+     */
+    void call(String number);
+}
